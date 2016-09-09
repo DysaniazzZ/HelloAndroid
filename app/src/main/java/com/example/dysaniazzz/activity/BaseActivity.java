510138ac2b41c24dbe97beacda2681a8779c80d1
DysaniazzZ <William.fzy@foxmail.com>
2016-09-09 21:50:55 +0800

    import the "logger" library

--- app/src/main/java/com/example/dysaniazzz/activity/BaseActivity.java
+++ app/src/main/java/com/example/dysaniazzz/activity/BaseActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.dysaniazzz.BuildConfig;
 import com.example.dysaniazzz.utils.ActivityCollector;
+import com.orhanobut.logger.LogLevel;
+import com.orhanobut.logger.Logger;
 
 /**
  * Created by fengzhenye on 2016/9/5.
@@ -12,19 +15,27 @@ import com.example.dysaniazzz.utils.ActivityCollector;
  */
 public class BaseActivity extends AppCompatActivity {
     
-    public static final String TAG = "BaseActivity";
     public Context mContext;
+    public static final String TAG = "BaseActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         init();
-        //Log.d(TAG, getClass().getSimpleName());
     }
     
     private void init() {
         mContext = this;
         ActivityCollector.getInstance().addActivity(this);
+
+        //init the log tag and don't show log for the release versions
+        if(BuildConfig.DEBUG) {
+            Logger.init(TAG).logLevel(LogLevel.FULL);   //for debug mode, print all log
+        } else {
+            Logger.init().logLevel(LogLevel.NONE);      //fot release mode, remove any log
+        }
+        Logger.d(getClass().getSimpleName());
+
         //EventBus.getDefault().register(this);
     }
 
