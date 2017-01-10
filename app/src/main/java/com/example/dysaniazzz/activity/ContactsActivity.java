dd944009e20ff798d019385c6ac2ce778774471f
DysaniazzZ <William.fzy@foxmail.com>
2017-01-10 22:42:25 +0800

    add the runtime permission when read contacts

--- app/src/main/java/com/example/dysaniazzz/activity/ContactsActivity.java
+++ app/src/main/java/com/example/dysaniazzz/activity/ContactsActivity.java
@@ -1,14 +1,19 @@
 package com.example.dysaniazzz.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.dysaniazzz.R;
+import com.example.dysaniazzz.utils.UIUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +33,7 @@ public class ContactsActivity extends BaseActivity {
 
     private Unbinder mUnbinder;
     private ArrayAdapter<String> mContactsAdapter;
-    private List<String> mContactsList = new ArrayList<String>();
+    private List<String> mContactsList = new ArrayList<>();
 
     public static void actionStart(Context context) {
         Intent intent = new Intent(context, ContactsActivity.class);
@@ -44,9 +49,14 @@ public class ContactsActivity extends BaseActivity {
     }
 
     private void init() {
-        mContactsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mContactsList);
+        mContactsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mContactsList);
         mLvContactsInfo.setAdapter(mContactsAdapter);
-        readContacts();
+        //动态获取联系人权限
+        if(ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+        } else {
+            readContacts();
+        }
     }
 
     private void readContacts() {
@@ -54,12 +64,15 @@ public class ContactsActivity extends BaseActivity {
         try {
             //查询联系人数据
             cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
-            while (cursor.moveToNext()) {
-                //获取联系人姓名
-                String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                //获取联系人手机号
-                String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                mContactsList.add(displayName + "\n" + number);
+            if(cursor != null) {
+                while (cursor.moveToNext()) {
+                    //获取联系人姓名
+                    String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    //获取联系人手机号
+                    String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    mContactsList.add(displayName + "\n" + number);
+                }
+                mContactsAdapter.notifyDataSetChanged();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -67,6 +80,21 @@ public class ContactsActivity extends BaseActivity {
             if (cursor != null) {
                 cursor.close();
             }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readContacts();
+                } else {
+                    UIUtils.createToast(mContext, "Your denied the read contact permission");
+                }
+                break;
+            default:
+                break;
         }
     }
 
